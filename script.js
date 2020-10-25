0f304f84392f87c6571e5512bb1299de9663f381
Vitalik <vitalia.karpiv@gmail.com>
2020-10-25 19:02:39 +0200

    add localStorage

--- script.js
+++ script.js
@@ -4,6 +4,37 @@ const addBtn = document.querySelector('.addBtn'),
       input = document.querySelector('input');
 
 addCloseBtn('li')
+reloadLocalStorage()
+//createItemListFromLocalStorage()
+
+
+/*function createItemListFromLocalStorage() {
+    for(let i = 0; i < 20; i++) {
+        if(localStorage.key(i))
+            console.log(localStorage.key(i))
+            const newLi = document.createElement('li')
+            newLi.textContent = localStorage.key(i);
+            list.appendChild(newLi)
+            addCloseBtn('li')
+    }
+}*/
+
+function cutX(str) {
+    for (let i = 0; i < 5; i++) {
+        if(str.slice(-1) == '\u00D7'){
+            str = str.slice(0, -1);
+        } 
+    }     
+    return str;  
+}
+
+function reloadLocalStorage() {
+    const items = document.querySelectorAll('li')
+
+    items.forEach(elem => {
+        localStorage.setItem(cutX(elem.textContent), elem.classList)
+    })
+}
 
 function addCloseBtn(listSelector) {
     const itemsList = document.querySelectorAll(listSelector);
@@ -15,23 +46,36 @@ function addCloseBtn(listSelector) {
     });
 }
 
+// adding new tasks
+
 addBtn.addEventListener('click', () => {
     const newLi = document.createElement('li')
     newLi.textContent = input.value;
     list.appendChild(newLi)
     addCloseBtn('li')
+    localStorage.setItem(cutX(newLi.textContent), '')
 })
+
+// Toggling class 'checked'
 
 list.addEventListener('click', (e) => {
     if(e.target && e.target.tagName == 'LI') {
         e.target.classList.toggle('checked')
+        if(localStorage.getItem(cutX(e.target.textContent)) == 'checked') {
+            localStorage.setItem(cutX(e.target.textContent), '')
+        } else {
+            localStorage.setItem(cutX(e.target.textContent), 'checked')
+        }
     }
 })
+
+// Deleting elements
 
 list.addEventListener('click', (e) => {
     if(e.target && e.target.tagName == 'SPAN') {
         console.log(e.target.parentElement);
         e.target.parentElement.style.display = 'none';
+        localStorage.removeItem(cutX(e.target.parentElement.textContent))
     }
 })
 
